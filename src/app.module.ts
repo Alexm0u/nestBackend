e40d3c54600e6212e5cb1d5d9a5ca4f85159013f
Álex <alejandro.mcamps@bosonit.com>
2023-10-02 09:14:19 +0200

    Variable de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, MongooseModule.forRoot('mongodb://localhost:27017')],
+  imports: [
+    AuthModule,
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    ConfigModule,
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log(process.env);
+  }
+}
